8969df911a7be3d367da20c725a906eb1e7f8b77
JuanMesino <juanmesinog@gmail.com>
2025-08-22 19:18:18 -0500

    delete backend

--- backend/services/hash.py
+++
@@ -1,9 +0,0 @@
-from passlib.context import CryptContext
-
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-def hash_password(contraseña: str) -> str:
-    return pwd_context.hash(contraseña)
-
-def verify_password(contraseña_plana: str, contraseña_hashed: str) -> bool:
-    return pwd_context.verify(contraseña_plana, contraseña_hashed)
